c9713cb468d7e4c5b01d4b5e639d438c01e67e40
Vj <vijay.mayekar04@gmail.com>
2017-09-05 00:05:13 +0530

    basic response & request

--- front.php
+++ front.php
@@ -1,3 +1,25 @@
 <?php
 
-require_once  'vendor/autoload.php'
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+$routes = array(
+	'/bye' => 'bye',
+	'/hello' => 'hello'
+);
+$current_path = $request->getPathInfo();
+if (array_key_exists($current_path, $routes)) {
+	ob_start();
+	extract($request->query->all(), EXTR_SKIP);
+	include sprintf(__DIR__ . '/src/pages/%s.php', $routes[$current_path]);
+	$response = new Response(ob_get_clean());
+}
+else {
+	$response = new Response('Page not found', 404);
+}
+
+$response->send();
