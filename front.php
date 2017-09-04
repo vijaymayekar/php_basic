1cedde1d76bf13b734227ed35cb9c84db3002cb0
Vj <vijay.mayekar04@gmail.com>
2017-09-05 00:38:59 +0530

    Add controller logic to routing

--- front.php
+++ front.php
@@ -16,11 +16,16 @@ $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
-try {
-	extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+function render_template($request) {
+	extract($request->attributes->all(), EXTR_SKIP);
 	ob_start();
 	include sprintf(__DIR__ . '/src/pages/%s.php', $_route);
-	$response = new Response(ob_get_clean());
+	return new Response(ob_get_clean());
+}
+
+try {
+	$request->attributes->add($matcher->match($request->getPathInfo()));
+	$response = call_user_func($request->attributes->get('_controller'), $request);
 }
 catch (RouteNotFoundException $e) {
 	$response = new Response('Page not found', 404);
--- src/app.php
+++ src/app.php
@@ -5,7 +5,10 @@ use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
 
-$routes->add('hello', new Route('/hello/{name}', array('name'=> 'World')));
-$routes->add('bye', new Route('/bye'));
+$routes->add('hello', new Route('/hello/{name}', array(
+	'name'=> 'World',
+	'_controller' => 'render_template'
+)));
+$routes->add('bye', new Route('/bye', array('_controller' => 'render_template')));
 
 return $routes;
