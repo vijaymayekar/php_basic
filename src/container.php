a3c0ae38cc19b293a7a486911b7d422c66e32457
Vj <vijay.mayekar04@gmail.com>
2017-09-10 13:09:56 +0530

    Add routes support

--- src/container.php
+++ src/container.php
@@ -1,38 +1,20 @@
 <?php
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader as DICLoader;
+use Symfony\Component\Routing\Loader\YamlFileLoader as RouteLoader;
 
 $sc = new DependencyInjection\ContainerBuilder();
 
 $sc->setParameter('charset', 'UTF-8');
-$sc->setParameter('routes', include __DIR__ . '/leap_year_app.php');
 
-$sc->register('context', 'Symfony\Component\Routing\RequestContext');
-$sc->register('matcher', 'Symfony\Component\Routing\Matcher\UrlMatcher')
-	->setArguments(array('%routes%', new Reference('context')))
-;
-$sc->register('resolver', 'Symfony\Component\HttpKernel\Controller\ControllerResolver');
+$locator = new FileLocator(__DIR__);
+$loader = new RouteLoader($locator);
+$routes = $loader->load('routes.yml');
+$sc->setParameter('routes', $routes);
 
-$sc->register('listener.router', 'Symfony\Component\HttpKernel\EventListener\RouterListener')
-	->setArguments(array(new Reference('matcher'), new RequestStack()))
-;
-$sc->register('listener.response', 'Symfony\Component\HttpKernel\EventListener\ResponseListener')
-	->setArguments(array('%charset%'))
-;
-$sc->register('listener.exception', 'Symfony\Component\HttpKernel\EventListener\ExceptionListener')
-	->setArguments(array('Calendar\\Controller\\ErrorController::exceptionAction'))
-;
-$sc->register('listener.string_response', 'Simplex\StringResponseListener');
-$sc->register('dispatcher', 'Symfony\Component\EventDispatcher\EventDispatcher')
-	->addMethodCall('addSubscriber', array(new Reference('listener.router')))
-	->addMethodCall('addSubscriber', array(new Reference('listener.response')))
-	->addMethodCall('addSubscriber', array(new Reference('listener.exception')))
-	->addMethodCall('addSubscriber', array(new Reference('listener.string_response')))
-;
-$sc->register('framework', 'Simplex\Framework')
-	->setArguments(array(new Reference('dispatcher'), new Reference('resolver')))
-;
+$loader = new DICLoader($sc, $locator);
+$loader->load('services.yml');
 
 return $sc;
